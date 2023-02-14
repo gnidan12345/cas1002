ee5bbc6af586e548e812a862fd08384b915c02a3
Yuliia <gnidan12345@gmail.com>
2023-02-14 17:37:04 +0200

    add webhook

---
+++ Test Suites/New Test Suite Webhook.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite Webhook</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>308f86a0-ab92-404c-a605-fec746f56f66</testSuiteGuid>
+</TestSuiteEntity>
